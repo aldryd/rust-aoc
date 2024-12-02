ed210a9ee88066d8e40cc148c73277b9a882a29a
Brock Atchison <aldryd@gmail.com>
2024-12-01 22:36:13 -0600

    Pull the common read file code into its own function

--- aoc-2024/src/main.rs
+++ aoc-2024/src/main.rs
@@ -2,15 +2,19 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
 
+/// Reads the input filename and returns a Vector of Strings for each line in the file
+fn read_lines(filename: &str) -> Vec<String> {
+    let file = File::open(filename);
+    let reader = io::BufReader::new(file.unwrap());
+
+    reader.lines().flatten().flat_map(|l| l.parse()).collect()
+}
+
 fn _day1_part1() {
     println!("--- Day 1: Historian Hysteria ---");
     println!("--- Part 1                    ---\n");
 
-    let input_path = "resources/day1_input.txt";
-
-    let file = File::open(input_path);
-    let reader = io::BufReader::new(file.unwrap());
-    let day1_input: Vec<String> = reader.lines().flatten().flat_map(|l| l.parse()).collect();
+    let day1_input = read_lines("resources/day1_input.txt");
 
     let mut col1: Vec<i32> = vec![];
     let mut col2: Vec<i32> = vec![];
@@ -31,18 +35,17 @@ fn _day1_part1() {
         total_distance += distance.abs();
     }
 
-    println!(">>>> total_distance: {total_distance}");
+    println!(">>>> total_distance: {}\n", total_distance);
+
+    // Keep track of the final answer for my input in case a refactor creates a bug
+    assert_eq!(total_distance, 2031679);
 }
 
 fn _day1_part2() {
     println!("--- Day 1: Historian Hysteria ---");
     println!("--- Part 2                    ---\n");
 
-    let input_path = "resources/day1_input.txt";
-
-    let file = File::open(input_path);
-    let reader = io::BufReader::new(file.unwrap());
-    let day1_input: Vec<String> = reader.lines().flatten().flat_map(|l| l.parse()).collect();
+    let day1_input = read_lines("resources/day1_input.txt");
 
     let mut col1: Vec<i32> = vec![];
     let mut col2_count_map: HashMap<i32, i32> = HashMap::new();
@@ -68,7 +71,10 @@ fn _day1_part2() {
         }
     }
 
-    println!(">>>> total_similarity:{}", total_similarity);
+    println!(">>>> total_similarity:{}\n", total_similarity);
+
+    // Keep track of the final answer for my input in case a refactor creates a bug
+    assert_eq!(total_similarity, 19678534);
 }
 
 fn main() {
@@ -90,6 +96,6 @@ fn main() {
     println!(".. .. ..................O000O........................ ...... ...");
     println!("... .. .......... Advent of Code 2024 ................... ... ..\n");
 
-    // _day1_part1();
+    _day1_part1();
     _day1_part2();
 }
